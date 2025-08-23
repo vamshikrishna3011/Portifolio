1a678368bebb5f5f2f99a0b43c49c0d88e284ce4
vamshikrishna3011 <vamshikrishna3834@gmail@.com>
2025-08-24 01:34:53 +0530

    refreshed

--- portifolio/settings.py
+++ portifolio/settings.py
@@ -133,7 +133,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vamshikrishnakannaji@gmail.com'  
-EMAIL_HOST_PASSWORD = 'moro zbly hevw buqv' 
+EMAIL_HOST_PASSWORD = 'wgxq ckzu ykik guut' 
 
 
 
